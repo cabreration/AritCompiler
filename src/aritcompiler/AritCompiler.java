c920ba5d0922d1913f85309ed7ddb1abd433bae3
Jav <j.a.cabrerapuente@gmail.com>
2020-03-24 20:50:16 -0600

    minor changes

--- src/aritcompiler/AritCompiler.java
+++ src/aritcompiler/AritCompiler.java
@@ -33,59 +33,7 @@ public class AritCompiler {
         // TODO code application logic here
         TextEditor txtE = new TextEditor();
         txtE.setVisible(true);
-        /*Node root = cupTry();
-        if (root != null) {
-            TreeProcesor.processFunctions(root);
-            ArrayList<Instruction> sentences = TreeProcesor.processTree(root);
-            SymbolsTable env = new SymbolsTable("global");
-            for (Instruction ins : sentences) {
-                if (ins != null)
-                    ins.process(env);
-            }
-            Singleton.print();
-        }*/
     }
-   
-    public static Node cupTry() {
-        BufferedReader bufferedReader;
-        String name = "C:\\Users\\jacab\\Documents\\Compi 2\\R-it\\Pruebas\\tree.arit";
-        try {
-            bufferedReader = new BufferedReader(new FileReader(name));
-            String parcial, completo= "";
-            parcial = bufferedReader.readLine();
-            while (parcial != null) 
-            {
-                completo += parcial +"\n";
-                parcial = bufferedReader.readLine();
-            }
-            
-            //System.out.println(completo);
-            bufferedReader.close();
-
-            /* Here we will try to parse it and see what does it generate */
-            Reader reader = new StringReader(completo); 
-            Scanner scanner = new Scanner(reader);
-            Parser parser = new Parser(scanner);
-            Symbol parse_tree = null;
-            
-            try {
-                //parse_tree = parser.debug_parse();
-                parse_tree = parser.parse();
-                Node root = parser.root;
-                TreePrinter.printTree(root, "cupTree");
-                return root;
-            }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
-    
     
     public static void javaccTry() {
         
